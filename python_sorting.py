9aba1425263f4e41c935ebe7aafaceb0fe8e6a41
Madhu_17 <maxmaddy7934@gmail.com>
2025-07-31 16:24:29 +0530

    created python file with sorting function.

---
+++ python_sorting.py
@@ -0,0 +1,72 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+def quick_sort(arr):
+    def _quick_sort(items, low, high):
+        if low < high:
+            pi = partition(items, low, high)
+            _quick_sort(items, low, pi-1)
+            _quick_sort(items, pi+1, high)
+
+    def partition(items, low, high):
+        pivot = items[high]
+        i = low - 1
+        for j in range(low, high):
+            if items[j] <= pivot:
+                i += 1
+                items[i], items[j] = items[j], items[i]
+        items[i+1], items[high] = items[high], items[i+1]
+        return i+1
+
+    _quick_sort(arr, 0, len(arr))
